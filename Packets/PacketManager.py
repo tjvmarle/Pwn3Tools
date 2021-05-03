66829f5994435d90bb3c5042d7b03534d5db690b
tjvanmarle <tjvanmarle@gmail.com>
2021-05-03 13:35:39 -0400

    Identified more packets. Made the filters easier to toggle.

--- Packets/PacketManager.py
+++ Packets/PacketManager.py
@@ -7,18 +7,30 @@ from importlib import reload
 # 0x7073: probably enemy positions?fireball
 filter = (
     0x0000,  # Heartbeat
-    0x1703,  # GH - shows periodically
     0x6d76,  # Position, also for enemies (?)
-    # 0x7073,
-    0x7878,  # SH - Terminating a spell projectile (?)
-    # 0x6d61,
+    0x6a70,  # GH - Jumping
+
     0x733d,  # SH - Switching weapons
     0x726c,  # SH - Reloading weapons
-    # 0x2a69,  # GH - Firing weapon
-    0x6a70,  # GH - Jumping
+    0x2a69,  # GH - Firing weapon/spell
+    0x6c61,  # SH - Gunshot firing
+    0x6672,  # SH - Gun projectile destruction (?) - Triggers with AK47, not with pistol/handgun
+    0x7878,  # SH - Terminating a spell projectile (?)
+
+    0x7073,  # SH - Rats position
+
+    0x2b2b,  # SH - Healing
+    0x6d61,  # SH - Mana (recharge)
+
+    # 0x6565,  # GH - Loot pickup
+    # 0x6370,  # SH - Loot pickup response
+
+    0x1703,  # GH - unk - shows periodically
+    # 0x6d6b,  # SH - Rat spawn
+    # 0x7374,  # SH - Rat spawn?
 )
 filter_client = (
-    "GH",
+    # "GH",
     # "SH",
 )
 
@@ -56,9 +68,15 @@ class PacketManager():
 
         packet_string = ""
         alternator = False
+        max = 50
         for byte in data:
+            if max == 0:
+                packet_string += "...."
+                break
             packet_string += format(byte, "x").rjust(2, "0") + (" " if alternator else "")
             alternator = not alternator
+            max -= 1
+
         print("unk", packet_string)
 
     def handle_packet(self, data):
